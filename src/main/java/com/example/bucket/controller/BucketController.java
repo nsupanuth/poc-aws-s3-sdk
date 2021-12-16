2eaf4493057a5c226a0769135317cfe1a7fa940b
SUPANUTH ONGSUK <supanuth.ongsuk@scbtechx.io>
2021-12-16 21:09:17 +0700

    poc s3 sdk

---
+++ src/main/java/com/example/bucket/controller/BucketController.java
@@ -0,0 +1,27 @@
+package com.example.bucket.controller;
+
+import com.example.bucket.service.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage/")
+public class BucketController {
+
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+
+}
